0122bcd197bbafd93e27d693538e6eccf540f945
Filip <szymanski.fp@gmail.com>
2024-09-25 00:04:37 +0200

    initial pinging

--- worker/worker.go
+++ worker/worker.go
@@ -2,30 +2,44 @@ package worker
 
 import (
 	"fmt"
+	"net/http"
 	"time"
 )
 
 //pobrać adresy do pingowania
 //zacząć goroutine który pinguje każdy url co 2min i zapisuje staty
 
+var client = &http.Client{
+	Timeout: 15 * time.Second,
+}
 
 func StartPinging(){
 	var adresses = getPingingAdresses()
 
 	for _, item := range adresses {
-		go ping(item)
+		ping(item)
 	}
 }
 
 func ping(ad string){
 	for {
-		fmt.Printf("Hello from item -> " + ad)
-		time.Sleep(5 * time.Second)
+		time.Sleep(1 * time.Second)
+		go performRequest(ad)
+	}
+}
+
+func performRequest(ad string) {
+	fmt.Println("SENDING REQUEST " + time.Now().Format("15:04:05"))
+	resp, err := client.Get(ad)
+	if err != nil {
+		fmt.Println("error")
+	} else {
+		fmt.Println("good " + resp.Status)
 	}
 }
 
 func getPingingAdresses() []string{
-	adresses := []string{"http://localhost:3000"}
+	adresses := []string{"http://localhost:3000/api/service/status"}
 	return adresses
 }
 
