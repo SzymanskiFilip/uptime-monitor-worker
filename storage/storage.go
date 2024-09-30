dd36b92bf4870d6048b9e0b954e57c57fa6ea92b
Filip <szymanski.fp@gmail.com>
2024-09-30 16:34:46 +0200

    initial response persistence

--- storage/storage.go
+++ storage/storage.go
@@ -1,8 +1,31 @@
 package storage
 
-import "net/http"
+import (
+	"fmt"
+	"log"
+	"net/http"
 
-func PersistRequest(r *http.Request){
+	"github.com/SzymanskiFilip/uptime-monitoring-go/types"
+)
+
+func PersistRequest(r *http.Response){
+	stat := types.Statistic {
+		URL: `http://localhost:3000`,
+		Headers: "any",
+		Status: int16(r.StatusCode),
+		Success: true,
+		ResponseTime: 200,
+	}
+
+	sqlStatement := `INSERT INTO statistics (url, headers, status, success, response_time) 
+	values ($1, $2, $3, $4, $5)`
+
+	_, err := db.Exec(sqlStatement, stat.URL, stat.Headers, stat.Status, stat.Success, stat.ResponseTime)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("persisted")
 
 }
 
