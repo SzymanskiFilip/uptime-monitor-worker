2b996d593c36dbbc62064195c735d056a7de1bfc
Filip <szymanski.fp@gmail.com>
2024-09-30 18:03:38 +0200

    urls table

--- storage/util.go
+++ storage/util.go
@@ -25,19 +25,31 @@ func InitializeDatabase(){
 		log.Fatal(err)
 	}
 
+	//DOMAINS TABLE
+	_, err = db.Exec(`
+	CREATE TABLE IF NOT EXISTS urls (
+		id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+		url TEXT NOT NULL UNIQUE
+	);
+	`)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	//STATISTICS TABLE
 	_, err = db.Exec(`
 	CREATE TABLE IF NOT EXISTS statistics (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    url TEXT NOT NULL,
+    url_id UUID NOT NULL,
     headers TEXT NOT NULL,
     status INT NOT NULL CHECK (status >= 100 AND status < 600),
     success BOOLEAN NOT NULL,
-    response_time INT NOT NULL CHECK (response_time >= 0)
+    response_time INT NOT NULL CHECK (response_time >= 0),
+	FOREIGN KEY (url_id) REFERENCES urls (id)
 	);
 	`)
 	if err != nil{
-		log.Fatal("error")
+		log.Fatal(err)
 	}
 
 
