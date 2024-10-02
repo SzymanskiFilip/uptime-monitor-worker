37d94ffd9ea0e600e3d662d0c7825bde3e1d512f
Filip <szymanski.fp@gmail.com>
2024-10-02 23:33:14 +0200

    next step

--- web/handlers.go
+++ web/handlers.go
@@ -7,4 +7,7 @@ func RegisterEndpoints(e *echo.Echo){
 	e.GET("/domains", GetRegisteredDomains)
 	e.POST("/domains", RegisterDomain)
 	e.DELETE("/domains", DeleteDomain)
+
+
+	//e.GET("/statistics")
 }
---
+++ web/statistics.go
@@ -0,0 +1,3 @@
+package web
+
+//funkcja ma zwracać response time średni z dnia i dzień na stronę główną
