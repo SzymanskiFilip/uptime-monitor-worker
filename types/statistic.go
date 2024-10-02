b76ed8cb031bdc4d2eb88fa8bb6f367a0c524537
Filip <szymanski.fp@gmail.com>
2024-10-02 23:05:07 +0200

    status not needed

--- types/statistic.go
+++ types/statistic.go
@@ -6,7 +6,6 @@ type Statistic struct {
 	Id uuid.UUID `json:"id"`
 	URL string `json:"url"`
 	Headers string `json:"headers"`
-	Status int16 `json:"status"`
 	Success bool `json:"success"`
 	ResponseTime int64 `json:"response_time"`
 }
