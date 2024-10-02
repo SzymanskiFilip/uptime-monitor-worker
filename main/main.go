3ba3e0517971ae139b36852792d4d00043de6cf0
Filip <szymanski.fp@gmail.com>
2024-10-02 12:00:28 +0200

    moved routes to separate file

--- main/main.go
+++ main/main.go
@@ -2,24 +2,22 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 
 	"github.com/SzymanskiFilip/uptime-monitoring-go/storage"
+	"github.com/SzymanskiFilip/uptime-monitoring-go/web"
 	"github.com/SzymanskiFilip/uptime-monitoring-go/worker"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
+	web.RegisterEndpoints(e)
 
 	fmt.Println("Program started...")
 	
 	storage.InitializeDatabase()
 
-	worker.StartPinging()
+	go worker.StartPinging()
 
 	e.Logger.Fatal(e.Start(":1323"))
 }
