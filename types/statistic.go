a64e43547b5b1710e0113fda67cbd1f1e3a85d03
Filip <szymanski.fp@gmail.com>
2024-10-03 23:38:05 +0200

    new struct mirroring db schema

--- types/statistic.go
+++ types/statistic.go
@@ -15,6 +15,15 @@ type Statistic struct {
 	SavedAt time.Time `json:"saved_at"`
 }
 
+type StatisticStored struct {
+	Id uuid.UUID `json:"id"`
+	URL_ID string `json:"url_id"`
+	Headers string `json:"headers"`
+	Success bool `json:"success"`
+	ResponseTime int64 `json:"response_time"`
+	SavedAt time.Time `json:"saved_at"`
+}
+
 
 type URLStored struct {
 	Id string `json:"id"`
